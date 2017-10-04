022588d65e56c686dacb7caf85df54524ebc0db0
Unknown <yiseca@gmail.com>
2017-10-04 08:37:45 -0500

    inicio del formulario

---
+++ resources/views/email.blade.php
@@ -0,0 +1,15 @@
+<html>
+
+
+
+<h2>Caminata RIO SAN FRANCISCO O VICACHA Domingo 01 de Octubre de 2017.</h2>
+
+<h3>Punto de encuentro: Plazoleta del Rosario, frente a la estación Museo del Oro. </h3>
+
+<h3>Hora de salida: 8:00am</h3>
+
+<h1>Su registro fue exitoso!</h1>
+
+
+
+<p></p>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,275 @@
+@extends('master')                              
+
+@section('content')         
+
+<link rel="stylesheet" type="text/css" href="public/Css/form.css">
+
+<link rel="stylesheet" type="text/css" href="public/Css/slider_styles.css">
+
+<section id="page1">
+
+    <div class="panel panel-default">
+
+        <div class="panel-heading">Inicio</div>
+
+        <div class="panel-body">
+
+            <div class="freebirdFormviewerViewFormContent ">
+
+                <div class="freebirdFormviewerViewHeaderHeader">
+
+                    <div class="freebirdFormviewerViewHeaderTitleRow">
+
+                        <div class="freebirdFormviewerViewHeaderTitle" dir="auto" role="heading" aria-level="1"><h3>RIO SAN FRANCISCO O VICACHA Domingo 01 de Octubre de 2017</h3></div>
+
+                    </div>
+<br>
+                    <div class="stage">
+
+                        <div id="SLDR-ONE" class="sldr">
+
+                            <ul class="wrp animate">
+
+                                <li class="elmnt-one">                                 
+
+                                        <div class="wrap"><img src="public/Img/rio.jpg"  height="400">
+
+                                        </div>                                   
+                                </li>
+
+                               <li class="elmnt-two">
+                                    
+                                        <div class="wrap"><img src="public/Img/rio1.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+
+                                 <li class="elmnt-three">
+                                    
+                                        <div class="wrap"><img src="public/Img/rio2.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+                                 
+
+                            </ul>
+
+                        </div>
+
+                        <div class="clear"></div>
+
+                        <ul class="selectors">
+
+                            <li class="focalPoint"><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>            
+                         
+                        </ul>
+
+                    </div>
+
+                    <script src="public/Js/jquery.sldr.js"></script>
+
+                    <div class="freebirdFormviewerViewHeaderDescription" dir="auto">
+
+                        <h3>Ubicación</h3>
+
+                        <p align="justify" style="font-size: 11pt; line-height: 25px">El sendero del río San Francisco o Vicachá está ubicado en la localidades de Candelaria y Santafé que marcaba el límite norte de Bogotá en sus primeros años de fundación. Nace en el páramo de Cruz Verde y baja por el canon entre los  cerros de Monserrate y Guadalupe. Al llegar a la ciudad en la Quinta de Bolívar es canalizado. En la actual avenida Jiménez con calle Sexta confluye por debajo de la tierra con el río San Agustín.</p>
+                       
+                        <h3>Descripción del recorrido </h3>
+
+                        <p align="justify" style="font-size: 11pt; line-height: 25px">El recorrido da inicio en la Plazoleta del Rosario ubicada frente a la estación Museo del oro, donde se realizara un calentamiento de aproximadamente 20 minutos, el guía de caminatas del IDRD dará las recomendaciones asi como las prohibiciones y restricciones propias del recorrido y necesarias para su ejecucion. Se dará inicio a la caminata desde la carrera 7ª con avenida Jimenez desplazándose hacia el oriente por el eje ambiental hasta llegar a la avenida circunvalar e ingresando a los predios del acueducto. En este sector se construyó un sendero por en bosque de niebla que conduce al mirador donde se puede apreciar el centro de la ciudad. El sendero en descenso nos lleva a la universidad distrital y de allí a la avenida Jiménez con carrera 3ª donde finaliza la caminata.</p>
+
+                        <h3>Durante el recorrido se visitaran los siguientes aspectos:</h3>
+                        <ul class="ul_inicio">
+                            <font style="font-size: 11pt; line-height: 25px;">
+                            <li>Mirador </li>
+                            <li>Históricos </li>
+                            <li>Culturales</li>
+                            <li>Arquitectónicos</li>
+                            <li>Hídricos</li>
+                            <li>Ambientales </li>
+                            <li>Ecosistema de niebla y sub - páramo</li>
+                            </font>
+                        </ul>
+                        <br>
+
+                        <font size="3"><center><table width="1000">
+
+                            <col width="281">
+
+                                <col width="282">
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Punto de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Plazoleta del Rosario, frente a la estación Museo del Oro.</p>
+
+                                        </td>
+
+                                    </tr>
+                              
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Fecha</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Domingo 01 de Octubre de 2017</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Hora de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>8:00 a.m</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Cupo Máximo</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>40 Personsa</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Rango de Edad</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>12 - 60 años</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Nivel</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Medio</p>
+
+                                        </td>
+
+                                    </tr>   
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Temperatura</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>14°C </p>
+
+                                        </td>
+
+                                    </tr> 
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Altitud</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>2.800 m.s.n.m</p>
+
+                                        </td>
+
+                                    </tr> 
+                       
+
+                        </table></center></font>
+
+                        <h3>Recomendaciones</h3>
+
+                        <ul class="ul_inicio">
+
+                            <font  style="font-size: 11pt; line-height: 25px">
+
+                                <li>Llevar ropa cómoda</li>
+                            
+                                <li>Llevar bloqueador solar</li>
+
+                                <li>Hidratación</li>
+
+                                <li>Calzado cómodo</li>
+
+                                <li>Llevar carné de la eps</li>
+
+                                <li>Refrigerio.</li>
+
+                                <li>Medicamentos personales </li>
+
+                                <li>No llevar mascotas </li>
+
+                                <li>Abstenerse de participar mujeres en estado de embarazo, personas que tengan problemas cardiacos y de rodillas y que sufran de asma y/o vértigo</li>
+
+                                <li><ins>Abstenerse de asistir si no se encuentra inscrito</ins></li>
+
+                                <!--<li><strong>Llevar la tarjeta de tu llave cargada, se debe regresar en el SITP</strong></li>-->
+                            </font>
+
+                            <br>
+
+                        </ul> <a class="btn btn-info" href="registro">Registrarse</a> </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</section>
+
+@stop
